ad99b52ba26e087754d5b901e73b67e035c652a8
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-11 12:45:23 +0200

    Arrange and Act preparing test

--- src/components/CityInfo/CityInfo.test.jsx
+++ src/components/CityInfo/CityInfo.test.jsx
@@ -1,9 +1,24 @@
-test('CityInfo render', async () => {
-    // Estándar AAA
-    // Arrange (arreglar, preparar, organizar todo lo que necesitamos)
+import React from "react";
+import { render } from "@testing-library/react";
+import CityInfo from "./CityInfo"; // SUT: Subject under Testing (objeto del testeo)
 
-    // Action (ejecutamos el test)
-    
-    // Assert (evaluar , preguntar)
+test("CityInfo render", async () => {
+  // Estándar AAA
+  // Arrange (arreglar, preparar, organizar todo lo que necesitamos)
 
-})
+  // Act (ejecutamos el test)
+
+  // Render: renderiza el componente y retorna una serie de funciones de utilidad
+  // En este caso 'findAllByRole'
+  const { findAllByRole } = render(
+    <CityInfo city="Badajoz" country="España" />
+  );
+
+  // Assert (evaluar , preguntar)
+  // findAllByRole nos va a buscar  ( en este caso ) todos los componentes que sean
+  // "heading" => H1, H2, H3, H4, H5, H6
+  // El resultado es un array de componentes. (cityAndCountryComponents)
+  const cityAndCountryComponents = await findAllByRole("heading");
+
+  console.log(cityAndCountryComponents);
+});

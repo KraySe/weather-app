b8fc4c690d7fa86351ed9f2a63bcfae8034a9024
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-13 09:39:27 +0200

    paramater state added

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Typography } from "@mui/material";
-import { WiCloud } from "react-icons/wi";
+import {
+  WiCloud,
+  WiDayCloudy,
+  WiDayFog,
+  WiDaySunny,
+  WiRain,
+} from "react-icons/wi";
 import { IconContext } from "react-icons/lib";
+
+const stateByName = {
+  cloud: WiCloud,
+  cloudy: WiDayCloudy,
+  fog: WiDayFog,
+  sunny: WiDaySunny,
+  rain: WiRain
+}
+
+const renderState = state => {
+  // TODO: implement function to render state by type
+}
 
 const Weather = ({ temperature }) => {
   return (
     <div>
       <IconContext.Provider value={{ size: "5em" }}>
-        <WiCloud style={{verticalAlign: "bottom"}}></WiCloud>
+        <WiCloud style={{ verticalAlign: "bottom" }}></WiCloud>
       </IconContext.Provider>
       <Typography display="inline" variant="h3">
         {temperature}

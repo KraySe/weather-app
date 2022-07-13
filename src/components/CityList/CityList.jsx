f33671ceac361a1b82d60567e73be1c1cd34ec20
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-13 18:05:16 +0200

    add event onClick to item on CityList

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -4,11 +4,11 @@ import Grid from "@mui/material/Grid";
 import CityInfo from "../CityInfo";
 import Weather from "../Weather";
 
-const renderCityAndCountry = (cityAndCoutry) => {
+const renderCityAndCountry = (eventOnClickCity) => (cityAndCoutry) => {
   const { city, country } = cityAndCoutry;
 
   return (
-    <li key={city}>
+    <li key={city} onClick={eventOnClickCity}>
       <Grid container justifyItems={"center"} alignItems={"center"}>
         <Grid item xs={12} md={8}>
           <CityInfo city={city} country={country} />
@@ -21,16 +21,19 @@ const renderCityAndCountry = (cityAndCoutry) => {
   );
 };
 
-const CityList = ({ cities }) => {
+const CityList = ({ cities, onClickCity }) => {
   return (
     <ul>
-      {cities.map((cityAndCoutry) => renderCityAndCountry(cityAndCoutry))}
+      {cities.map((cityAndCoutry) =>
+        renderCityAndCountry(onClickCity)(cityAndCoutry)
+      )}
     </ul>
   );
 };
 
 CityList.propTypes = {
   cities: PropTypes.array.isRequired,
+  onClickCity: PropTypes.func.isRequired,
 };
 
 export default CityList;
--- src/components/CityList/CityList.test.jsx
+++ src/components/CityList/CityList.test.jsx
@@ -27,8 +27,6 @@ test("CityList renders", async () => {
 });
 
 test("CityList click on item", async () => {
-  // Debemos simular una acción del usuario: click sobre un item
-  // Para eso vamos a utilizar una función "mock"
   const fnClickOnItem = jest.fn();
 
   const { findAllByRole } = render(
@@ -37,13 +35,7 @@ test("CityList click on item", async () => {
 
   const items = await findAllByRole("listitem");
 
-  // Ahora, para simular la acción vamos a utilizar fireEvent
-  // fireEvent es parte de la librearía de testing-library/react
-
   fireEvent.click(items[0]);
-
-  // ¿Ahora que sucede?
-  // Se debió llamar a la función fnClickOnItem UNA única vez
 
   expect(fnClickOnItem).toHaveBeenCalledTimes(1);
 });

6b1638dfafcda0c37565b8924ef8623ebec58a65
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-13 11:06:30 +0200

    CityList component init

---
+++ src/components/CityList/CityList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import CityInfo from "../CityInfo/CityInfo";
+import Weather from "../Weather/Weather";
+
+const renderCityAndCountry = (cityAndCoutry) => {
+  const { city, country } = cityAndCoutry;
+
+  return (
+    <li>
+      <CityInfo city={city} country={country} />
+      <Weather temperature={10} />
+    </li>
+  );
+};
+
+const CityList = ({ cities }) => {
+  return (
+    <ul>
+      {cities.map((cityAndCoutry) => renderCityAndCountry(cityAndCoutry))}
+    </ul>
+  );
+};
+
+CityList.propTypes = {
+  cities: PropTypes.array.isRequired,
+};
+
+export default CityList;
---
+++ src/components/CityList/CityList.test.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import CityList from "./CityList";
+
+test("CityList render", async () => {
+  // TODO: implement test AAA
+});

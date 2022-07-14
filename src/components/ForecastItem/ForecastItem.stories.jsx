b59d9055935893af22b5c7352cc5c7abf8d55400
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-14 09:58:54 +0200

    ForecastItem component init

---
+++ src/components/ForecastItem/ForecastItem.stories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ForecastItem from "./ForecastItem";
+
+export default {
+  title: "ForecastItem",
+  component: ForecastItem,
+};
+
+export const SunnyMonday = () => (
+  <ForecastItem
+    weekDay="Monday"
+    hour="09:43"
+    state="sunny"
+    temperature={23}
+  />
+);
---
+++ src/components/ForecastItem/ForecastItem.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ForecastItem from './ForecastItem';
+import { render } from "@testing-library/react";
+
+test("ForecastItem render", async () => {
+    //TODO: Implement test
+});

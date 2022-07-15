4cbfcf534a727e871e9f0e8f7a64e9d0c8ee4d2c
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-15 11:10:22 +0200

    Create pages

---
+++ src/pages/WelcomePages.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const WelcomePages = (props) => {
+  return <div>WelcomePages</div>;
+};
+
+WelcomePages.propTypes = {};
+
+export default WelcomePages;

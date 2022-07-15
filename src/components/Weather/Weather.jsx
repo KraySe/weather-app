a0cd45bd4503e0162a905e385df373b3794685f8
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-15 14:29:21 +0200

    visual improve to Weather component

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { IconContext } from "react-icons/lib";
 import IconState, { validValues } from "../IconState";
 
-
 const Weather = ({ temperature, state }) => {
   return (
-    <div>
+    <Grid
+      container
+      item
+      direction={"row"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      spacing={1}
+    >
       <IconContext.Provider value={{ size: "5em" }}>
         <IconState state={state} />
       </IconContext.Provider>
-      <Typography display="inline" variant="h3">
+      <Typography display="inline" variant="h2">
         {temperature}
       </Typography>
-    </div>
+    </Grid>
   );
 };
 
 Weather.propTypes = {
   temperature: PropTypes.number.isRequired,
-  state: PropTypes.oneOf(validValues).isRequired
+  state: PropTypes.oneOf(validValues).isRequired,
 };
 
 export default Weather;

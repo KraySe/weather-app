5acbd88ee718cac3f78c1ac7ebbd2615badb5b52
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-15 13:27:13 +0200

    Improve structure to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Grid } from "@mui/material";
 import WelcomePage from "./pages/WelcomePage";
 import MainPage from "./pages/MainPage";
 import CityPage from "./pages/CityPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-const App = (props) => {
+const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-      <Router>
-        <Switch>
-          <Route exact path={"/"}>
-            <WelcomePage />
-          </Route>
-          <Route path={"/main"}>
-            <MainPage />
-          </Route>
-          <Route path={"/city"}>
-            <CityPage />
-          </Route>
-          <Route>
-            <NotFoundPage />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Grid container justifyContent={"center"} direction={"row"}>
+      <Grid item sm={10}>
+        <h1>App</h1>
+        <Router>
+          <Switch>
+            <Route exact path={"/"}>
+              <WelcomePage />
+            </Route>
+            <Route path={"/main"}>
+              <MainPage />
+            </Route>
+            <Route path={"/city"}>
+              <CityPage />
+            </Route>
+            <Route>
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </Router>
+      </Grid>
+    </Grid>
   );
 };
 

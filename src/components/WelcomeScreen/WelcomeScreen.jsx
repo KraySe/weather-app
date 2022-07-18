5ef330a8b55422ab365d443337352a0a1fb79ac2
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-18 10:16:15 +0200

    Activate Clouds effect

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import Clouds from "vanta/dist/vanta.clouds.min";
+import * as THREE from "three";
 
 const WelcomeScreen = ({ children }) => {
   const myRefDiv = useRef(null); // initial value, first render
@@ -11,7 +13,11 @@ const WelcomeScreen = ({ children }) => {
     console.log("myRefDiv.current (useEffect)", myRefDiv.current);
 
     if (!vanta) {
-      setVanta(1);
+      setVanta(Clouds({
+        THREE,
+        el: myRefDiv.current,
+      }));
+
       console.log("set new value to vanta");
     }
   }, [vanta]);

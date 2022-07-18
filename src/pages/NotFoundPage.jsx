7e053463d31d81410a76c98f14cf6ce6a85eb670
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-18 11:56:52 +0200

    Improve Not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,15 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import { Grid, Typography, Link } from "@mui/material";
+import { IconContext } from "react-icons";
+import { WiRain } from "react-icons/wi";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      {" "}
-      Not Found
-      <div>
-        <Link to="/main">Volver a Main</Link>
+    <Grid
+      container
+      direction={"column"}
+      justifyContent={"center"}
+      className={"full"}
+    >
+      <div className="highlight">
+        <Grid
+          item
+          container
+          xs={12}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Grid item>
+            <IconContext.Provider value={{ size: "6em" }}>
+              <WiRain />
+            </IconContext.Provider>
+          </Grid>
+          <Grid
+            item
+            container
+            direction={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Typography variant={"h4"} color={"inherit"}>
+              404 | Not found page
+            </Typography>
+            <Link
+              color={"inherit"}
+              aria-label={"menu"}
+              component={RouterLink}
+              to={"/main"}
+            >
+              Return to start
+            </Link>
+          </Grid>
+        </Grid>
       </div>
-    </div>
+    </Grid>
   );
 };
 

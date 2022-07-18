7898896f73f9d556f691e3b40ed73d7a8008a189
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-18 09:49:23 +0200

    useEffect test

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,10 +1,14 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const WelcomeScreen = ({ children }) => {
   const myRefDiv = useRef(null); // initial value, first render
 
   console.log("myRefDiv.current", myRefDiv.current);
+
+  useEffect(() => {
+    console.log("myRefDiv.current (useEffect)", myRefDiv.current);
+  });
 
   return <div ref={myRefDiv}>WelcomeScreen</div>;
 };

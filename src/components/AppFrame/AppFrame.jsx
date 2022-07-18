a94a7674bc1b01eeea31845cb8211d9c28c822f7
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-18 08:55:12 +0200

    AppFrame structure

--- src/components/AppFrame/AppFrame.jsx
+++ src/components/AppFrame/AppFrame.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link as LinkRouter } from "react-router-dom";
+import {
+  Grid,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Link,
+  Typography,
+} from "@mui/material";
+import { IconContext } from "react-icons";
+import { WiDaySunny } from "react-icons/wi";
 
-const AppFrame = props => {
+const AppFrame = (props) => {
   return (
-    <div>AppFrame</div>
-  )
-}
+    <Grid container justifyContent={"center"}>
+      <AppBar position={"static"}>
+        <Toolbar variant={"dense"}>
+          <IconButton color={"inherit"} aria-label={"menu"}>
+            <Link
+              component={LinkRouter}
+              to={"/main"}
+              color={"inherit"}
+              aria-label={"menu"}
+            >
+              <IconContext.Provider value={{ size: "2em" }}>
+                <WiDaySunny />
+              </IconContext.Provider>
+            </Link>
+          </IconButton>
+          <Typography variant={"h6"} color={"inherit"}>
+            Weather App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container item xs={12} sm={11} md={10} bg={8}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
+        consequuntur dolores tenetur deserunt numquam vitae magnam molestias ea
+        et veritatis. Neque consequuntur iure voluptate nesciunt. Cumque totam
+        voluptates culpa iusto.
+      </Grid>
+    </Grid>
+  );
+};
 
-AppFrame.propTypes = {}
+AppFrame.propTypes = {};
 
-export default AppFrame
+export default AppFrame;

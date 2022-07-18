b83c6760fd3491fbafdb91ff414bf511a19a3e16
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-18 09:35:16 +0200

    WelcomeScreen init

---
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const WelcomeScreen = ({ children }) => {
+  return <div>WelcomeScreen</div>;
+};
+
+WelcomeScreen.propTypes = {
+    children: PropTypes.node,
+};
+
+export default WelcomeScreen;

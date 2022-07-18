95a6c90ab4e588ed5cf9ec40b3b528e80478858e
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-18 11:38:47 +0200

    add center zone to WelcomePage

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Grid } from "@mui/material";
 import WelcomeScreen from "./../components/WelcomeScreen";
 
 const WelcomePage = () => {
   return (
     <WelcomeScreen>
-
+      <Grid
+        container
+        direction={"column"}
+        justifyContent={"center"}
+        className={"full"}
+      >
+        <div className="highlight"></div>
+      </Grid>
     </WelcomeScreen>
   );
 };

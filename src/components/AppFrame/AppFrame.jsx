6a9b3af95be379444a0f52949534c1e0b4008308
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-18 08:39:39 +0200

    AppFrame Component init

---
+++ src/components/AppFrame/AppFrame.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AppFrame = props => {
+  return (
+    <div>AppFrame</div>
+  )
+}
+
+AppFrame.propTypes = {}
+
+export default AppFrame

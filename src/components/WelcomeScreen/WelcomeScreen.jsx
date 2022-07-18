b381ac2d9033e3596596cb08014e901979dcea09
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-18 10:01:46 +0200

    interaction useState with useEffect

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,13 +1,19 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 const WelcomeScreen = ({ children }) => {
   const myRefDiv = useRef(null); // initial value, first render
+  const [vanta, setVanta] = useState(0); // initial value
 
   console.log("myRefDiv.current", myRefDiv.current);
 
   useEffect(() => {
     console.log("myRefDiv.current (useEffect)", myRefDiv.current);
+
+    if (!vanta) {
+      setVanta(1);
+      console.log("set new value to vanta");
+    }
   });
 
   return <div ref={myRefDiv}>WelcomeScreen</div>;

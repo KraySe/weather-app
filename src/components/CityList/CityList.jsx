4d84751e6b1e9faf6a8595d130cc59f6f1872296
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-20 20:02:58 +0200

    async / await - refactoring

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -31,36 +31,34 @@ const CityList = ({ cities, onClickCity }) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const setWeather = (city, country, countryCode) => {
+    const setWeather = async (city, country, countryCode) => {
       const appid = "8548ce8ec745e1ea8db3cfed3bcbadd6";
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${appid}`;
-      axios
-        .get(url)
-        .then((response) => {
-          const { data } = response;
-          const temperature = Number(
-            convertUnits(data.main.temp).from("K").to("C").toFixed(0)
-          );
-          const state = data.weather[0].main.toLowerCase();
+      const response = await axios.get(url);
+      
+      try {
+        const { data } = response;
+        const temperature = Number(
+          convertUnits(data.main.temp).from("K").to("C").toFixed(0)
+        );
+        const state = data.weather[0].main.toLowerCase();
 
-          const propName = `${city}-${country}`;
-          const propValue = { temperature, state };
+        const propName = `${city}-${country}`;
+        const propValue = { temperature, state };
 
-          setAllWeather((allWeather) => ({
-            ...allWeather,
-            [propName]: propValue,
-          }));
-        })
-        .catch((error) => {
-          if (error.response) {
-            const { data, status } = error.response;
-            setError("ups!! it seems that an error has occurred");
-          } else if (error.request) {
-            setError("ups!! check your internet connection");
-          } else {
-            setError("ups!! problem loading data");
-          }
-        });
+        setAllWeather((allWeather) => ({
+          ...allWeather,
+          [propName]: propValue,
+        }));
+      } catch (error) {
+        if (error.response) {
+          setError("ups!! it seems that an error has occurred");
+        } else if (error.request) {
+          setError("ups!! check your internet connection");
+        } else {
+          setError("ups!! problem loading data");
+        }
+      }
     };
 
     cities.forEach(({ city, country, countryCode }) => {

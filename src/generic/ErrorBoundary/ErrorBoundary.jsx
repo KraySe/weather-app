73e381133131d48ada3fcf96395a46c61aa1952d
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-21 11:27:23 +0200

    Error Boundary update

--- src/generic/ErrorBoundary/ErrorBoundary.jsx
+++ src/generic/ErrorBoundary/ErrorBoundary.jsx
@@ -5,44 +5,17 @@ class ErrorBoundary extends React.Component {
     super(props);
 
     this.state = {
-      active: false,
+      hasError: false,
     };
   }
 
-  isActive = () => {
-    return this.state.active ? "Active" : "Inactive";
-  };
-
-  onClickHandler = () => {
-    this.setState({
-      active: true,
-    });
-  };
-
-  componentDidMount() {
-    console.log("The component has been mounted");
+  // this.setState(getDerivedStateFromError(error))
+  static getDerivedStateFromError(error) {
+     return { hasError: true }
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("prev state", prevState.active);
-    console.log("new state", this.state.active);
-    console.log("The component has been updated");
-  }
-
-  componentWillUnmount() {
-    console.log("The component has been Unmounted");
-   }
 
   render() {
-    return (
-      <div>
-        <button onClick={this.onClickHandler}>Activate</button>
-        <h1>
-          ErrorBoundary {this.props.greeting}
-          {this.isActive()}
-        </h1>
-      </div>
-    );
+    return this.state.hasError ? <h1>There was a mistake</h1> : this.props.children;
   }
 }
 
--- src/generic/ErrorBoundary/ErrorBoundary.stories.jsx
+++ src/generic/ErrorBoundary/ErrorBoundary.stories.jsx
@@ -6,6 +6,18 @@ export default {
   component: ErrorBoundary,
 };
 
-export const ErrorBoundaryExample = () => (
-  <ErrorBoundary greeting={"Hello"} />
+const prop = undefined;
+const ComponentWithError = () => <h1>{prop.hello}</h1>;
+const ComponentWithoutError = () => <h1>Without Error</h1>;
+
+export const ErrorBoundaryWithError = () => (
+  <ErrorBoundary>
+    <ComponentWithError />
+  </ErrorBoundary>
+);
+
+export const ErrorBoundaryWithoutError = () => (
+  <ErrorBoundary>
+    <ComponentWithoutError />
+  </ErrorBoundary>
 );

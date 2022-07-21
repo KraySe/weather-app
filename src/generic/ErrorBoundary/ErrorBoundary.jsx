393699ee2261c771882845b6504db243ddeba04c
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-21 09:50:31 +0200

    use props in the class

--- src/generic/ErrorBoundary/ErrorBoundary.jsx
+++ src/generic/ErrorBoundary/ErrorBoundary.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 class ErrorBoundary extends React.Component {
   render() {
-    return <h1>ErrorBoundary</h1>;
+    return <h1>ErrorBoundary {this.props.greeting}</h1>;
   }
 }
 

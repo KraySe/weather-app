691c3b849a98c8c5a9c8a6c6494524a8ad6e6c72
jcjaramago <jcjaramagoreyes@gmail.com>
2022-07-25 22:23:14 +0200

    Refactoring - useVanta hook

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,37 +1,15 @@
-import React, { useRef, useEffect, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import Clouds from "vanta/dist/vanta.clouds.min";
-import * as THREE from "three";
+import useVanta from "../../hooks/useVanta";
 
 const WelcomeScreen = ({ children }) => {
-  const myRefDiv = useRef(null); // initial value, first render
-  const [vanta, setVanta] = useState(0); // initial value
+  const { myRefDiv } = useVanta();
 
-  console.log("myRefDiv.current", myRefDiv.current);
-
-  useEffect(() => {
-    console.log("myRefDiv.current (useEffect)", myRefDiv.current);
-
-    if (!vanta) {
-      setVanta(
-        Clouds({
-          THREE,
-          el: myRefDiv.current,
-        })
-      );
-
-      console.log("set new value to vanta");
-    }
-
-    return () => {
-      if (vanta) {
-        vanta.destroy();
-        console.log("clean resources");
-      }
-    };
-  }, [vanta]);
-
-  return <div className={"full"} ref={myRefDiv}>{children}</div>;
+  return (
+    <div className={"full"} ref={myRefDiv}>
+      {children}
+    </div>
+  );
 };
 
 WelcomeScreen.propTypes = {

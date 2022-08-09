60352e32bd2ba659b3f631d00ed29ad15ce88020
jcjaramago <jcjaramagoreyes@gmail.com>
2022-08-09 10:26:55 +0200

    optimize app with useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import React, { useReducer } from "react";
+import React, { useCallback, useReducer } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import MainPage from "./pages/MainPage";
 import CityPage from "./pages/CityPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-const App = () => {
-  const initialValue = {
-    allWeather: {},
-    allChartData: {},
-    allForecastItemList: {},
-  };
+const initialValue = {
+  allWeather: {},
+  allChartData: {},
+  allForecastItemList: {},
+};
 
-  const reducer = (state, action) => {
+const App = () => {
+  const reducer = useCallback((state, action) => {
     switch (action.type) {
       case "SET_ALL_WEATHER":
         const weatherCity = action.payload;
-        const newAllWeather = { ...state.allWeather, ...weatherCity }
-        return { ...state, allWeather: newAllWeather};
+        const newAllWeather = { ...state.allWeather, ...weatherCity };
+        return { ...state, allWeather: newAllWeather };
       case "SET_CHART_DATA":
         const chartDataCity = action.payload;
         const newChartDataCity = { ...state.chartDataCity, ...chartDataCity };
         return { ...state, allChartData: newChartDataCity };
       case "SET_FORECAST_ITEM_LIST":
         const forecastItemListCity = action.payload;
-        const newForecastItemListCity = { ...state.forecastItemList, ...forecastItemListCity };
+        const newForecastItemListCity = {
+          ...state.forecastItemList,
+          ...forecastItemListCity,
+        };
         return { ...state, allForecastItemList: newForecastItemListCity };
       default:
         return state;
     }
-  };
+  }, []);
 
   const [state, dispatch] = useReducer(reducer, initialValue);
 
